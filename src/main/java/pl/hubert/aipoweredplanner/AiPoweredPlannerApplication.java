c132bb4f3fec6ab4a8430f63624a44c05e18b29c
Hubert Jakubowski <h.j.jakubowski@gmail.com>
2026-05-27 21:24:24 +0200

    Initial commit

---
+++ src/main/java/pl/hubert/aipoweredplanner/AiPoweredPlannerApplication.java
@@ -0,0 +1,13 @@
+package pl.hubert.aipoweredplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPoweredPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiPoweredPlannerApplication.class, args);
+    }
+
+}
